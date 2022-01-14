3000e37c32712fb454af867b0da82bef57756c0e
Muhammad Waleed <mujahidwaleed11@gmail.com>
2022-01-14 11:59:15 +0500

    changes: jest demo added

--- src/components/JestDemo/jestDemo.test.tsx
+++ src/components/JestDemo/jestDemo.test.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
-import JestDemo from '.';
+import React from 'react'
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import JestDemo from '.'
 
-let container: HTMLDivElement | null = null;
+let container: HTMLDivElement | null = null
 
 beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
 
 afterEach(() => {
   if (container) {
     // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
+    unmountComponentAtNode(container)
+    container.remove()
+    container = null
   }
-});
+})
 
 // it('renders correctly', () => {
 //   act(() => {
@@ -31,26 +31,26 @@ it('renders user data', async () => {
   const fakeUser = {
     name: 'Joni Baez',
     age: '32',
-  };
+  }
 
-  const spiedFetch = jest.spyOn(global, 'fetch');
+  const spiedFetch = jest.spyOn(global, 'fetch')
 
   spiedFetch.mockImplementation(() =>
     Promise.resolve({
       json: () => Promise.resolve(fakeUser),
     } as Response),
-  );
+  )
 
   // Use the asynchronous version of act to apply resolved promises
   await act(async () => {
-    render(<JestDemo id="123" />, container);
-  });
+    render(<JestDemo id="123" />, container)
+  })
 
-  console.log('container --', container?.innerHTML);
+  console.log('container --', container?.innerHTML)
 
-  expect(container?.querySelector('h1')?.textContent).toBe(fakeUser.name);
-  expect(container?.querySelector('h2')?.textContent).toBe(fakeUser.age);
+  expect(container?.querySelector('h1')?.textContent).toBe(fakeUser.name)
+  expect(container?.querySelector('h2')?.textContent).toBe(fakeUser.age)
 
   // remove the mock to ensure tests are completely isolated
-  spiedFetch.mockRestore();
-});
+  spiedFetch.mockRestore()
+})

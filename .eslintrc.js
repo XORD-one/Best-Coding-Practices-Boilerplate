719f2347238806ed165b415900efd9d54fd28fbf
Bilal Ahmad <bilal.ahmad.n28@gmail.com>
2022-03-14 16:16:52 +0500

    changes: hook for action creators

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,20 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 13,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": "error",
   },
-}
+};
--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,32 +1,32 @@
 // * Contains Business Logic and returns view component
-import React, { FC } from 'react'
+import React, { FC } from "react";
 
-import { useAppDispatch, useAppSelector } from '@hooks/index'
-import { updateCounterValue } from '@redux/slices/counterSlice/counter.actions'
-import CounterView from '@components/Counter/view'
+import { useActions, useAppSelector } from "@hooks/index";
+import CounterView from "@components/Counter/view";
 
-import './styles.scss'
+import "./styles.scss";
 
 export interface CounterProps {
-  title?: string
+  title?: string;
 }
 
-const Counter: FC<CounterProps> = ({ title = 'Counter Compo' }) => {
-  const counterValue = useAppSelector(state => state.counter.value)
-  const dispatch = useAppDispatch()
+const Counter: FC<CounterProps> = ({ title = "Counter Compo" }) => {
+  const counterValue = useAppSelector((state) => state.counter.value);
+
+  const { updateCounterValue } = useActions();
 
   return (
     <CounterView
       title={title}
       counterValue={counterValue}
       onIncrementPress={() => {
-        dispatch(updateCounterValue('inc'))
+        updateCounterValue("inc");
       }}
       onDecrementPress={() => {
-        dispatch(updateCounterValue('dec'))
+        updateCounterValue("dec");
       }}
     />
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
